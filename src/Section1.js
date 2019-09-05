b51967adac558b85ae71da898bfd2fe9f106b4e2
RanranLiu111 <864749024@qq.com>
2019-09-05 11:25:39 +0800

    设置表单样式

--- src/Section1.js
+++ src/Section1.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import { makeStyles,createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 
@@ -32,14 +33,31 @@ const useStyles = makeStyles(theme => ({
     padding:32
   },
   form:{
-    width:500
+    width:500,
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'center',
+    padding:'32px 16px'
   },
   padding:{
     padding:8
   },
+  inputOuterStyle:{
+    width:'100%',
+    display:'block',
+    marginTop:16
+  },
+  inputRoot:{
+    width:'100%',
+  },
   inputStyle:{
-    width:300,
-    padding:'9px 5px'
+    padding:'9px 5px',
+  },
+  inputLabelOutlined:{
+    transform:'translate(14px, 12px) scale(1)'
+  },
+  stateWrapper:{
+    alignItems:'flex-end'
   }
 }));
 
@@ -66,27 +84,89 @@ export default function Section1(){
           </Grid>
           <Grid item xs={6} className={classes.content}>
             <Paper className={classes.form}>
+              <Typography variant="h5"  className={classes.padding}>
+                Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+              </Typography>
               <ThemeProvider theme={theme}>
                 <TextField
-                  label="ThemeProvider"
+                  label="Url"
                   variant="outlined"
                   classes={{
-                    root:classes.inputStyle
+                    root:classes.inputOuterStyle
                   }}
                   InputProps={{
                     classes:{
+                      root:classes.inputRoot,
                       input:classes.inputStyle
+                    }
+                  }}
+                  InputLabelProps={{
+                    classes:{
+                      outlined:classes.inputLabelOutlined
                     }
                   }}
                 />
               </ThemeProvider>
-              <Typography variant="h5"  className={classes.padding}>
-                Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-              </Typography>
-              <Typography variant="h5"  className={classes.padding}>
+              <Grid container className={classes.stateWrapper} spacing={3}>
+                <Grid item xs={6} >
+                  <ThemeProvider theme={theme}>
+                    <TextField
+                      label="OS"
+                      variant="outlined"
+                      classes={{
+                        root:classes.inputOuterStyle
+                      }}
+                      InputProps={{
+                        classes:{
+                          root:classes.inputRoot,
+                          input:classes.inputStyle
+                        }
+                      }}
+                      InputLabelProps={{
+                        classes:{
+                          outlined:classes.inputLabelOutlined
+                        }
+                      }}
+                    />
+                  </ThemeProvider>
+                </Grid>
+                <Grid item xs={6} >
+                  <ThemeProvider theme={theme}>
+                    <TextField
+                      label="State"
+                      variant="outlined"
+                      classes={{
+                        root:classes.inputOuterStyle
+                      }}
+                      InputProps={{
+                        classes:{
+                          root:classes.inputRoot,
+                          input:classes.inputStyle
+                        }
+                      }}
+                      InputLabelProps={{
+                        classes:{
+                          outlined:classes.inputLabelOutlined
+                        }
+                      }}
+                    />
+                  </ThemeProvider>
+                </Grid>
+              </Grid>
+              <Grid container className={classes.stateWrapper} spacing={3} style={{marginTop:12}}>
+                <Grid item xs={6}>
+                  <Button variant="contained" color="secondary" style={{width:'100%',color:'white'}}>
+                    Submit
+                  </Button>
+                </Grid>
+                <Grid item xs={6}>
+                    <a style={{fontSize:12,color:'#5abebe',cursor:'pointer'}}>Click to get more >></a>
+                </Grid>
+              </Grid>
+              <Typography variant="body1"  className={classes.padding} style={{marginTop:16}}>
                 Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
               </Typography>
-              <Typography variant="h5"  className={classes.padding}>
+              <Typography variant="body1"  className={classes.padding}>
                 Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
               </Typography>
             </Paper>
